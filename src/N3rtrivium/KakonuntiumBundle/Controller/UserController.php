172df984b37da3d460bc93bc2b3f6b9c1d9c5726
nohum <eiskalterengel@truthfactory.tk>
2014-05-17 10:55:35 +0200

    routing engine: typos fixed

--- src/N3rtrivium/KakonuntiumBundle/Controller/UserController.php
+++ src/N3rtrivium/KakonuntiumBundle/Controller/UserController.php
@@ -4,6 +4,7 @@ namespace N3rtrivium\KakonuntiumBundle\Controller;
 
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations\Post;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends FOSRestController
 {
@@ -11,8 +12,10 @@ class UserController extends FOSRestController
     /**
      * @Post("/users")
      */ 
-    public function createUserAction()
+    public function createUserAction(Request $request)
     {
+	    $rawJsonData = $request->getContent();
+
         $userService = $this->container->get('n3rtrivium_kakonuntium.service.users');
         $userService->createUser();
     }
