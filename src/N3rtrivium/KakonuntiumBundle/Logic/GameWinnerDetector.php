543017376baa525295f9f5759a279ec6e675110e
nohum <eiskalterengel@truthfactory.tk>
2014-05-24 16:38:56 +0200

    winner calculation

--- src/N3rtrivium/KakonuntiumBundle/Logic/GameWinnerDetector.php
+++ src/N3rtrivium/KakonuntiumBundle/Logic/GameWinnerDetector.php
@@ -3,6 +3,7 @@
 namespace N3rtrivium\KakonuntiumBundle\Logic;
 
 use Doctrine\Common\Collections\Collection;
+use N3rtrivium\KakonuntiumBundle\Entity\Guess;
 use N3rtrivium\KakonuntiumBundle\Entity\User;
 
 /**
@@ -13,6 +14,8 @@ class GameWinnerDetector
 	private $actualCountings;
 
 	private $userGuesses;
+
+	private $usersById;
 
 	public function __construct(array $countings, Collection $guesses)
 	{
@@ -25,7 +28,74 @@ class GameWinnerDetector
 	 */
 	public function determineWinners()
 	{
-		// TODO: choose a winner
-		return null;
+		$result = array();
+		$countingsPerUser = $this->doCountings();
+
+		foreach ($this->actualCountings as $which => $actualCount)
+		{
+			if (!isset($countingsPerUser[$which]))
+			{
+				$result[$which] = null;
+				continue;
+			}
+
+			// if there are no countings, there is no winner!
+			if (!$actualCount)
+			{
+				$result[$which] = null;
+				continue;
+			}
+
+			$smallestDiff = null;
+			$currentWinnerUserId = null;
+			foreach ($countingsPerUser[$which] as $userId => $guessNumberOfUser)
+			{
+				$diff = abs($actualCount - $guessNumberOfUser);
+				if ($smallestDiff === null || $diff < $smallestDiff)
+				{
+					$smallestDiff = $diff;
+					$currentWinnerUserId = $userId;
+				}
+			}
+
+			if ($currentWinnerUserId !== null)
+			{
+				$currentWinnerUserId = $this->usersById[$currentWinnerUserId];
+			}
+
+			$result[$which] = $currentWinnerUserId;
+		}
+
+		return $result;
+	}
+
+	private function doCountings()
+	{
+		$separatedGuessingNumbers = array();
+
+		foreach ($this->userGuesses as $guess)
+		{
+			/** @var $guesses Guess */
+
+			// fill usersById array to find users again later
+			if (!isset($this->usersById[$guess->getUser()->getId()]))
+			{
+				$this->usersById[$guess->getUser()->getId()] = $guess->getUser();
+			}
+
+			if (!isset($separatedGuessingNumbers[$guess->getWhich()]))
+			{
+				$separatedGuessingNumbers[$guess->getWhich()] = array();
+			}
+
+			if (!isset($separatedGuessingNumbers[$guess->getWhich()][$guess->getUser()->getId()]))
+			{
+				$separatedGuessingNumbers[$guess->getWhich()][$guess->getUser()->getId()] = 0;
+			}
+
+			$separatedGuessingNumbers[$guess->getWhich()][$guess->getUser()->getId()]++;
+		}
+
+		return $separatedGuessingNumbers;
 	}
 }
